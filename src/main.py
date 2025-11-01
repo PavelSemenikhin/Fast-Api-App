89c064b291b8cd08c8d5bb2ea30ae266fcb5f7b3
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-02 01:48:23 +0200

    Add prefix from config

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from api import router as api_router
+from core.config import settings
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+app = FastAPI(prefix="/app")
+
+app.include_router(api_router, prefix=settings.api.prefix)
